0f50f3b8803b52a188e61db94b101a62c5a6722b
Carlos Lavin <115796142+CarlosLavin1@users.noreply.github.com>
2024-03-04 16:31:47 -0400

    added project skeleton (#1)

---
+++ DiningRoomManager/src/main/java/com/example/diningroommanager/DiningRoomManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.diningroommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningRoomManagerApplication.class, args);
+    }
+
+}
